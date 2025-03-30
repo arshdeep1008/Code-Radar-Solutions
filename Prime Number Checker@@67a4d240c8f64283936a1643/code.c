0baf842ec04ca088cc39d3b5c67c2529d09b6a52
arshdeep1008 <arshdeepsingh.sohana@gmail.com>
2025-03-30 16:41:39 +0530

    Solution to Prime Number Checker

--- Prime Number Checker@@67a4d240c8f64283936a1643/code.c
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -1,3 +1,4 @@
+#include<stdio.h>
 def is_prime(n):
     if n <= 1:  # Numbers less than or equal to 1 are not prime
         return False
@@ -6,6 +7,7 @@ def is_prime(n):
             return False  # If divisible by i, not prime
     return True  # If no divisors found, number is prime
 
-# Example usage
+int main{
 number = 29
 print(f"{number} is a prime number: {is_prime(number)}")
+}
